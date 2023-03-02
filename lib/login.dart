911dade84073cee99565b2ff6e551c4f8246e783
Cenachi <carloscenachi@protonmail.com>
2023-03-02 12:25:48 -0300

    snackBar

--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,11 @@
+import 'dart:convert';
+
+import 'package:auth/home.dart';
 import 'package:flutter/material.dart';
 import 'package:email_validator/email_validator.dart';
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -15,13 +21,20 @@ class _LoginState extends State<Login> {
 
   String _errorMessage = '';
 
+  final snackBar = const SnackBar(
+    content: Text(
+      'Email ou senha inválidos!',
+      textAlign: TextAlign.center,
+    ),
+    backgroundColor: Colors.green,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Align(
-        alignment: Alignment.center,
-        child: Form(
-          key: _formKey,
+      body: Form(
+        key: _formKey,
+        child: Center(
           child: SingleChildScrollView(
             padding: EdgeInsets.symmetric(horizontal: 16),
             child: Column(children: [
@@ -30,12 +43,45 @@ class _LoginState extends State<Login> {
                 decoration: const InputDecoration(labelText: 'Email'),
                 keyboardType: TextInputType.emailAddress,
                 onChanged: (email) {
-                  validateEmail(email);
+                  String res = validateEmail(email);
+                },
+                validator: (email) {
+                  if (email == null || email.isEmpty) {
+                    return 'Por favor, digite seu email!';
+                  } else {
+                    return 'Digite seu email correto';
+                  }
                 },
               ),
-              TextFormField(),
+              TextFormField(
+                controller: _passwordController,
+                decoration: const InputDecoration(labelText: 'Password'),
+                keyboardType: TextInputType.text,
+                validator: (senha) {
+                  if (senha == null || senha.isEmpty) {
+                    return 'Por favor, digite sua senha!';
+                  } else if (senha.length < 6) {
+                    return 'Senha incorreta';
+                  }
+                  return null;
+                },
+              ),
               ElevatedButton(
-                onPressed: () {},
+                onPressed: () async {
+                  FocusScopeNode currentFocus = FocusScope.of(context);
+                  if (_formKey.currentState!.validate()) {
+                    bool res = await login();
+                    if (!currentFocus.hasPrimaryFocus) {
+                      currentFocus.unfocus();
+                    }
+                    if (res) {
+                      Get.to(const Home());
+                    } else {
+                      _passwordController.clear();
+                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    }
+                  }
+                },
                 child: const Text('ENTRAR'),
               ),
             ]),
@@ -45,7 +91,7 @@ class _LoginState extends State<Login> {
     );
   }
 
-  void validateEmail(String email) {
+  String validateEmail(String email) {
     if (email.isEmpty) {
       setState(() {
         _errorMessage = "Email cannot be empty.";
@@ -57,5 +103,27 @@ class _LoginState extends State<Login> {
         // print(_errorMessage);
       });
     }
+    return _errorMessage;
+  }
+
+  Future<bool> login() async {
+    SharedPreferences sharedPreference = await SharedPreferences.getInstance();
+    var url = Uri.parse('http://10.0.2.2:8080/api/auth/login');
+    try {
+      var response = await http.post(url, body: {
+        'email': _emailController.text,
+        'password': _passwordController.text,
+      });
+
+      if (response.statusCode == 200) {
+        print(jsonDecode(response.body)['token']);
+        return true;
+      }
+      print(jsonDecode(response.body));
+    } catch (e) {
+      print(e);
+    }
+
+    return false;
   }
 }
